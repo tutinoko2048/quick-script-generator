14a2d3afbbf63c7b7eeba5a83fe80dcb3e86f3e0
RetoRuto9900K <41989402+tutinoko2048@users.noreply.github.com>
2022-12-27 14:08:53 +0900

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <textarea style={{ width: "80%"; maxHeight: "300px" }}></textarea>
+        <textarea style={{ width: "80%", maxHeight: "300px" }}></textarea>
       </main>
     </>
   )
